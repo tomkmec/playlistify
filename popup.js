0893db1e1b8735237748b0ee6e4c68ebe262e459
Tom Kmec <tom.kmec@gmail.com>
2016-01-27 00:26:32 +0100

    Its alive

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ function onGo() {
   $.each(tracks, function(i, t) {
     t = t.trim();
     if (t != '') {
-      $('#step2').append($('<p>').html(t + '...').data('raw', t));
+      $('#step2').append($('<p>').html(t + '...').data('raw', t).addClass('pending'));
     }
   });
 
@@ -28,7 +28,7 @@ function onAuthComplete(responseURL) {
 
   var startMarker = '#access_token=';
   var token = responseURL.substring(responseURL.indexOf(startMarker) + startMarker.length)
-  token = token.substring(0, token.indexOf('&'));
+  window.token = token.substring(0, token.indexOf('&'));
 
   $('#step2 p').each(function() {
     var $track = $(this);
@@ -43,9 +43,79 @@ function onSearchResult($track) {
   return function(data) {
     if (data && data.tracks && data.tracks.items && data.tracks.items.length > 0) {
       var track = data.tracks.items[0];
-      $track.empty().append($('<a>').attr({'href': track.href}).html(track.artists[0].name + ': ' + track.name))
+      $track.empty()
+        .append($('<a>').attr({'href': track.href}).html(track.artists[0].name + ': ' + track.name))
+        .data('spotify-uri', track.uri);
+      $track.removeClass('pending').addClass('found');
     } else {
-      $track.addClass('na')
+      $track.addClass('na').removeClass('pending');
     }
+    if ($('p.pending').length == 0) onAllSearchDone();
   }
+}
+
+function onAllSearchDone() {
+  console.log('all search done, about to fetch identity');
+
+  $.ajax({
+      url: 'https://api.spotify.com/v1/me',
+      type: 'GET',
+      headers: {
+          'Authorization': 'Bearer ' + window.token
+      },
+      dataType: 'json',
+      success: onIdentityFetched
+  });
+}
+
+function onIdentityFetched(iddata) {
+  console.log('identity fetched', iddata);
+
+  var url = 'https://api.spotify.com/v1/users/{user_id}/playlists';
+  url = url.replace('{user_id}', iddata.id);
+  window.userId = iddata.id;
+  var data = {
+    name: $('#playlistName').val()
+  };
+
+  console.log('about to create a playlist', url, data);
+
+  $.ajax({
+      url: url,
+      type: 'POST',
+      data: JSON.stringify(data),
+      headers: {
+          'Authorization': 'Bearer ' + window.token
+      },
+      dataType: 'json',
+      success: onPlaylistCreated
+  });
+}
+
+function onPlaylistCreated(playlistData) {
+  console.log('playlist created', playlistData);
+
+  var uris = $('#step2 p.found').map(function(i, p) { return $(p).data('spotify-uri') }).toArray();
+
+  var url = 'https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks'
+  url = url.replace('{user_id}', window.userId);
+  url = url.replace('{playlist_id}', playlistData.id);
+
+  console.log('about to add songs', url, uris);
+
+  $.ajax({
+      url: url,
+      type: 'POST',
+      data: JSON.stringify({'uris' : uris}),
+      headers: {
+          'Authorization': 'Bearer ' + window.token
+      },
+      dataType: 'json',
+      success: done
+  });
+}
+
+function done(data) {
+  //window.close();
+  console.log('done', data);
 }
