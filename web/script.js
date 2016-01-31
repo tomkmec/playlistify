74f1e963c49ec209960376bdd1a9606280194d27
Tom Kmec <tom.kmec@gmail.com>
2016-01-31 01:06:49 +0100

    web version functioning up to track search

--- web/script.js
+++ web/script.js
@@ -1,9 +1,119 @@
 $(function() {
-  var clientId = '12eaf8ebd98c4528bc53118dd56d4a1c';
-  var redirectURI = 'http://tom-kmec.net/playlistify/callback.html'
-  var context = {};
+  var context = this;
 
-  $('#go').on('click', go);
+  this.clientId = '12eaf8ebd98c4528bc53118dd56d4a1c';
+  this.redirectURI = 'http://localhost/pl'// 'http://tom-kmec.net/playlistify/callback.html'
+
+  this.init = function init() {
+    var accessTokenMatch = location.hash.match(/access_token=([^&]+)/);
+    if (accessTokenMatch) {
+      context.accessToken = accessTokenMatch[1];
+      $('#step2').show();
+      context.getUserInfo();
+      $('#search').on('click', context.doSearch);
+    } else {
+      $('#step1').show();
+      $('#start').on('click', function() {
+        var url = 'https://accounts.spotify.com/authorize?client_id={clientId}&response_type=token&redirect_uri={redirectURI}&scope={scope}';
+        url = url.replace('{clientId}', context.clientId);
+        url = url.replace('{redirectURI}', context.redirectURI);
+        url = url.replace('{scope}', 'playlist-modify-public user-read-private');
+        window.location.href = url;
+      });
+    }
+  };
+
+  this.getUserInfo = function getUserInfo() {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        type: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + context.accessToken
+        },
+        dataType: 'json',
+        success: function(data) { 
+          context.userData = data;
+          $('h2:visible').html('Hello ' + data.display_name + ', drop your playlist here please');
+          if (data.images && data.images.length > 0) {
+            $('.mdl-card__menu:visible').append($('<img>').attr({'src':data.images[0].url}).addClass('avatar'));
+          }
+          console.log(data);
+          $('#search').removeAttr('disabled');
+          $('.mdl-spinner:visible').hide();
+        }
+    });
+  }
+
+  this.doSearch = function doSearch() {
+    var tracks = $('#in').val().split('\n');
+    $.each(tracks, function(i, t) {
+      t = t.trim();
+      if (t != '') {
+        $('#step3 tbody')
+          .append($('<tr>').addClass('pending').data('raw', t)
+            .append($('<td colspan="4">')
+              .addClass('mdl-data-table__cell--non-numeric')
+              .attr('colspan', 4)
+              .html('Searching for '+ t + '...')));
+      }
+    });
+
+    if ($('tbody tr').length > 0) {
+      $('#step2').fadeOut();
+      $('#step3').fadeIn();
+      $('tbody tr').each(function() {
+        var $track = $(this);
+        $.ajax({
+          url: 'https://api.spotify.com/v1/search',
+          type: 'GET',
+          data: {
+            'type': 'track', 
+            'q': $track.data('raw')
+            //,'market': 'from_token'
+          },
+          headers: {
+              'Authorization': 'Bearer ' + context.accessToken
+          },
+          dataType: 'json',
+          success: context.onSearchResult($track)
+        });
+      })
+    }
+  }
+
+  this.onSearchResult = function onSearchResult($track) {
+    return function(data) {
+      if (data && data.tracks && data.tracks.items && data.tracks.items.length > 0) {
+        var track = data.tracks.items[0];
+        var tdClass = "mdl-data-table__cell--non-numeric";
+        $track.empty()
+          .append($('<td>').addClass(tdClass).html(track.artists[0].name))
+          .append($('<td>').addClass(tdClass).html(track.name))
+          .append($('<td>').addClass(tdClass).html(track.album.name))
+          .append($('<td>'));
+                    //  <button id="alts1" class="mdl-button mdl-js-button mdl-button--icon" style="top:-6px;">
+                    //   <i class="material-icons">more_vert</i>
+                    // </button>
+                    // <span class="mdl-badge" data-badge="1" style="left: -20px"></span>
+                    // <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="alts1">
+                    //   <li class="mdl-menu__item">Some Action: Some ActionSome Action</li>
+                    //   <li class="mdl-menu__item">Another Action</li>
+                    //   <li disabled class="mdl-menu__item">Disabled Action</li>
+                    //   <li class="mdl-menu__item">Yet Another Action</li>
+                    // </ul>
+        $track.removeClass('pending').addClass('found');
+      } else {
+        $track.addClass('na').removeClass('pending').find("td").html('could not find "' + $track.data('raw') + '"');
+      }
+      if ($('p.pending').length == 0) context.onAllSearchDone();
+    }
+  }
+
+  this.onAllSearchDone = function onAllSearchDone() {
+
+  }
+
+  this.init();
 })
 
 function go() {
@@ -21,11 +131,11 @@ function go() {
   //   $('#step2').show();
   // }
 
-  var url = 'https://accounts.spotify.com/authorize?client_id={clientId}&response_type=token&redirect_uri={redirectURI}&scope={scope}';
-  url = url.replace('{clientId}', '12eaf8ebd98c4528bc53118dd56d4a1c');
-  url = url.replace('{redirectURI}', encodeURI('https://' + chrome.runtime.id + '.chromiumapp.org/index.html'));
-  url = url.replace('{scope}', 'playlist-modify-public');
-  $.featherlight({iframe: url, iframeMaxWidth: '80%', iframeWidth: 500, iframeHeight: 300});  
+  // var url = 'https://accounts.spotify.com/authorize?client_id={clientId}&response_type=token&redirect_uri={redirectURI}&scope={scope}';
+  // url = url.replace('{clientId}', '12eaf8ebd98c4528bc53118dd56d4a1c');
+  // url = url.replace('{redirectURI}', encodeURI('https://' + chrome.runtime.id + '.chromiumapp.org/index.html'));
+  // url = url.replace('{scope}', 'playlist-modify-public');
+  // $.featherlight({iframe: url, iframeMaxWidth: '80%', iframeWidth: 500, iframeHeight: 300});  
 
   //chrome.identity.launchWebAuthFlow({ url : url, interactive: true}, onAuthComplete)
 }
