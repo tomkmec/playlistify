3d19690bd883acb6d079a48d7825963ab10a1d68
Tom Kmec <tom.kmec@gmail.com>
2016-02-01 01:13:28 +0100

    funkcne finalni web 1.0

--- web/script.js
+++ web/script.js
@@ -2,7 +2,7 @@ $(function() {
   var context = this;
 
   this.clientId = '12eaf8ebd98c4528bc53118dd56d4a1c';
-  this.redirectURI = 'http://localhost/pl'// 'http://tom-kmec.net/playlistify/callback.html'
+  this.redirectURI = 'http://tom-kmec.net/playlistify'
 
   this.init = function init() {
     var accessTokenMatch = location.hash.match(/access_token=([^&]+)/);
@@ -17,7 +17,7 @@ $(function() {
         var url = 'https://accounts.spotify.com/authorize?client_id={clientId}&response_type=token&redirect_uri={redirectURI}&scope={scope}';
         url = url.replace('{clientId}', context.clientId);
         url = url.replace('{redirectURI}', context.redirectURI);
-        url = url.replace('{scope}', 'playlist-modify-public user-read-private');
+        url = url.replace('{scope}', 'playlist-modify-private playlist-modify-public user-read-private');
         window.location.href = url;
       });
     }
@@ -68,8 +68,9 @@ $(function() {
           type: 'GET',
           data: {
             'type': 'track', 
-            'q': $track.data('raw')
-            //,'market': 'from_token'
+            'q': $track.data('raw'),
+            'limit': 10,
+            'market': 'from_token'
           },
           headers: {
               'Authorization': 'Bearer ' + context.accessToken
@@ -85,22 +86,41 @@ $(function() {
     return function(data) {
       if (data && data.tracks && data.tracks.items && data.tracks.items.length > 0) {
         var track = data.tracks.items[0];
+        var rowId = 'track' + $track.index();
         var tdClass = "mdl-data-table__cell--non-numeric";
+
+        var $optionsTD = $('<td>');
+        if (data.tracks.items.length > 1) {
+          var $optionsLI = [];
+          $.each(data.tracks.items, function() {
+            var altData = this;
+            var desc = this.artists[0].name + ': ' + this.name + ' (' + this.album.name + ')';
+            $optionsLI.push($('<li>').addClass("mdl-menu__item").html(desc).data('spotify-uri', altData.uri).on('click', function() {
+              $track.find('td')
+                .first().html(altData.artists[0].name)
+                .next().html(altData.name)
+                .next().html(altData.album.name)
+              // $(this).siblings().removeAttr('disabled');
+              // $(this).attr('disabled', 'disabled');
+              $track.data('spotify-uri', altData.uri)
+            }));
+            // $optionsLI[0].attr('disabled', 'disabled')
+          })
+          $optionsTD
+            .append($('<button>').addClass("mdl-button mdl-js-button mdl-button--icon").css('top', '-6px').attr('id', rowId)
+              .append($('<i>').addClass("material-icons").html("more_vert"))
+            ).append($('<span>').addClass("mdl-badge").attr('data-badge', data.tracks.items.length-1).css('left','-14px'))
+            .append($('<ul>').addClass("mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect").attr('for', rowId)
+              .append($optionsLI)
+            )
+        }
+
         $track.empty()
           .append($('<td>').addClass(tdClass).html(track.artists[0].name))
           .append($('<td>').addClass(tdClass).html(track.name))
           .append($('<td>').addClass(tdClass).html(track.album.name))
-          .append($('<td>'));
-                    //  <button id="alts1" class="mdl-button mdl-js-button mdl-button--icon" style="top:-6px;">
-                    //   <i class="material-icons">more_vert</i>
-                    // </button>
-                    // <span class="mdl-badge" data-badge="1" style="left: -20px"></span>
-                    // <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="alts1">
-                    //   <li class="mdl-menu__item">Some Action: Some ActionSome Action</li>
-                    //   <li class="mdl-menu__item">Another Action</li>
-                    //   <li disabled class="mdl-menu__item">Disabled Action</li>
-                    //   <li class="mdl-menu__item">Yet Another Action</li>
-                    // </ul>
+          .append($optionsTD)
+          .data('spotify-uri', track.uri)
         $track.removeClass('pending').addClass('found');
       } else {
         $track.addClass('na').removeClass('pending').find("td").html('could not find "' + $track.data('raw') + '"');
@@ -110,130 +130,53 @@ $(function() {
   }
 
   this.onAllSearchDone = function onAllSearchDone() {
+          componentHandler.upgradeDom();
+      $('#done').removeAttr('disabled').on('click', context.createPlaylist);
+  }
 
+  this.createPlaylist = function createPlaylist() {
+    var url = 'https://api.spotify.com/v1/users/{user_id}/playlists';
+    url = url.replace('{user_id}', context.userData.id);
+    var data = {
+      name: $('#playlistName').val(),
+      'public': $('#playlistPublic').parent('label').is('.is-checked')
+    };
+
+    if (!context.done) {
+      context.done = true;
+      $.ajax({
+          url: url,
+          type: 'POST',
+          data: JSON.stringify(data),
+          headers: {
+              'Authorization': 'Bearer ' + context.accessToken
+          },
+          dataType: 'json',
+          success: context.populatePlaylist
+      });
+    }
+  }
+
+  this.populatePlaylist = function populatePlaylist(playlistData) {
+    var uris = $('tr.found').map(function(i, p) { return $(p).data('spotify-uri') }).toArray();
+
+    var url = 'https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks'
+    url = url.replace('{user_id}', context.userData.id);
+    url = url.replace('{playlist_id}', playlistData.id);
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: JSON.stringify({'uris' : uris}),
+        headers: {
+            'Authorization': 'Bearer ' + context.accessToken
+        },
+        dataType: 'json',
+        success: function() {
+          window.location.href = playlistData.external_urls.spotify
+        }
+    });
   }
 
   this.init();
 })
-
-function go() {
-  // var tracks = $('#input').val().split('\n');
-
-  // $.each(tracks, function(i, t) {
-  //   t = t.trim();
-  //   if (t != '') {
-  //     $('#step2').append($('<p>').html(t + '...').data('raw', t).addClass('pending'));
-  //   }
-  // });
-
-  // if ($('#step2 p').length > 0) {
-  //   $('#step1').hide();
-  //   $('#step2').show();
-  // }
-
-  // var url = 'https://accounts.spotify.com/authorize?client_id={clientId}&response_type=token&redirect_uri={redirectURI}&scope={scope}';
-  // url = url.replace('{clientId}', '12eaf8ebd98c4528bc53118dd56d4a1c');
-  // url = url.replace('{redirectURI}', encodeURI('https://' + chrome.runtime.id + '.chromiumapp.org/index.html'));
-  // url = url.replace('{scope}', 'playlist-modify-public');
-  // $.featherlight({iframe: url, iframeMaxWidth: '80%', iframeWidth: 500, iframeHeight: 300});  
-
-  //chrome.identity.launchWebAuthFlow({ url : url, interactive: true}, onAuthComplete)
-}
-
-/*
-function onAuthComplete(responseURL) {
-
-  var startMarker = '#access_token=';
-  var token = responseURL.substring(responseURL.indexOf(startMarker) + startMarker.length)
-  window.token = token.substring(0, token.indexOf('&'));
-
-  $('#step2 p').each(function() {
-    var $track = $(this);
-    $.getJSON(
-      'https://api.spotify.com/v1/search', 
-      {'type': 'track', 'q': $track.data('raw'), 'market': 'CZ'}, 
-      onSearchResult($track));
-  })
-}  
-
-function onSearchResult($track) {
-  return function(data) {
-    if (data && data.tracks && data.tracks.items && data.tracks.items.length > 0) {
-      var track = data.tracks.items[0];
-      $track.empty()
-        .append($('<a>').attr({'href': track.href}).html(track.artists[0].name + ': ' + track.name))
-        .data('spotify-uri', track.uri);
-      $track.removeClass('pending').addClass('found');
-    } else {
-      $track.addClass('na').removeClass('pending');
-    }
-    if ($('p.pending').length == 0) onAllSearchDone();
-  }
-}
-
-function onAllSearchDone() {
-  console.log('all search done, about to fetch identity');
-
-  $.ajax({
-      url: 'https://api.spotify.com/v1/me',
-      type: 'GET',
-      headers: {
-          'Authorization': 'Bearer ' + window.token
-      },
-      dataType: 'json',
-      success: onIdentityFetched
-  });
-}
-
-function onIdentityFetched(iddata) {
-  console.log('identity fetched', iddata);
-
-  var url = 'https://api.spotify.com/v1/users/{user_id}/playlists';
-  url = url.replace('{user_id}', iddata.id);
-  window.userId = iddata.id;
-  var data = {
-    name: $('#playlistName').val()
-  };
-
-  console.log('about to create a playlist', url, data);
-
-  $.ajax({
-      url: url,
-      type: 'POST',
-      data: JSON.stringify(data),
-      headers: {
-          'Authorization': 'Bearer ' + window.token
-      },
-      dataType: 'json',
-      success: onPlaylistCreated
-  });
-}
-
-function onPlaylistCreated(playlistData) {
-  console.log('playlist created', playlistData);
-
-  var uris = $('#step2 p.found').map(function(i, p) { return $(p).data('spotify-uri') }).toArray();
-
-  var url = 'https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks'
-  url = url.replace('{user_id}', window.userId);
-  url = url.replace('{playlist_id}', playlistData.id);
-
-  console.log('about to add songs', url, uris);
-
-  $.ajax({
-      url: url,
-      type: 'POST',
-      data: JSON.stringify({'uris' : uris}),
-      headers: {
-          'Authorization': 'Bearer ' + window.token
-      },
-      dataType: 'json',
-      success: done
-  });
-}
-
-function done(data) {
-  //window.close();
-  console.log('done', data);
-}
-*/
